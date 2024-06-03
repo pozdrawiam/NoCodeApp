3aeb9ac5d980075954e007e8a39bb43484cb7c54
Dawid Tr <witam@tuta.io>
2024-06-03 22:12:58 +0200

    DataValue entity refactor

--- Nca.Domain/Entities/Values/DataValue.cs
+++ Nca.Domain/Entities/Values/DataValue.cs
@@ -6,7 +6,7 @@ public class DataValue : BaseEntity
 {
     public int DataDefinitionId { get; set; }
     
-    public virtual DataDefinition? DataDefinition { get; set; }
-    
+    public virtual DataDefinition DataDefinition { get; set; } = null!;
+
     public virtual ICollection<FieldValue> Fields { get; set; } = new List<FieldValue>();
 }
