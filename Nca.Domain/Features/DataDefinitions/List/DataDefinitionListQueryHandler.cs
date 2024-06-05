da9d4c954c452795702948f0d551cdf0afe401bd
Dawid Tr <witam@tuta.io>
2024-06-05 22:22:54 +0200

    Definition list query test

--- Nca.Domain/Features/DataDefinitions/List/DataDefinitionListQueryHandler.cs
+++ Nca.Domain/Features/DataDefinitions/List/DataDefinitionListQueryHandler.cs
@@ -1,6 +1,7 @@
 namespace Nca.Domain.Features.DataDefinitions.List;
 
-public class DataDefinitionListQueryHandler : IQueryHandler<DataDefinitionListQuery, DataDefinitionListQueryResult>
+public class DataDefinitionListQueryHandler(IDb db) 
+    : IQueryHandler<DataDefinitionListQuery, DataDefinitionListQueryResult>
 {
     public Task<DataDefinitionListQueryResult> ExecuteAsync(DataDefinitionListQuery query)
     {
---
+++ Nca.Tests/Domain/Features/DataDefinitions/DataDefinitionListQueryHandlerTests.cs
@@ -0,0 +1,53 @@
+using Nca.Domain.Entities.Definitions;
+using Nca.Domain.Features.DataDefinitions.List;
+
+namespace Nca.Tests.Domain.Features.DataDefinitions;
+
+public class DataDefinitionListQueryHandlerTests
+{
+    private readonly DataDefinitionListQueryHandler _handler;
+    private readonly IDb _db = TestHelper.CreateInMemoryDb();
+
+    public DataDefinitionListQueryHandlerTests()
+    {
+        _handler = new DataDefinitionListQueryHandler(_db);
+    }
+
+    [Fact]
+    public async Task Should_load_list_from_db()
+    {
+        var dataDefinition1 = new DataDefinition
+        {
+            Id = 1,
+            Name = "Def1",
+            Fields = new List<FieldDefinition>
+            {
+                new() { Id = 1, Name = "A", Sequence = 0 }
+            }
+        };
+        
+        var dataDefinition2 = new DataDefinition
+        {
+            Id = 2,
+            Name = "Def2",
+            Fields = new List<FieldDefinition>
+            {
+                new() { Id = 2, Name = "B", Sequence = 0 },
+                new() { Id = 3, Name = "C", Sequence = 1 }
+            }
+        };
+
+        _db.DataDefinitions.Add(dataDefinition1);
+        _db.DataDefinitions.Add(dataDefinition2);
+        await _db.SaveChangesAsync();
+
+        var query = new DataDefinitionListQuery();
+        
+        // Act
+        var result = await _handler.ExecuteAsync(query);
+        
+        Assert.Equal(2, result.Entries.Count);
+        Assert.Equal("Def1", result.Entries.First().Name);
+        Assert.Equal("Def2", result.Entries.ElementAt(1).Name);
+    }
+}
