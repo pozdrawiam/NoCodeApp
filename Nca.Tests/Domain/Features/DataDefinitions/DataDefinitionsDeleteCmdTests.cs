b9a6ba5dfc0db071fb0086d14405cb6a61d0c38b
David <witam@tuta.io>
2024-06-07 22:24:16 +0200

    Complete delete test implementation

--- Nca.Tests/Domain/Features/DataDefinitions/DataDefinitionsDeleteCmdTests.cs
+++ Nca.Tests/Domain/Features/DataDefinitions/DataDefinitionsDeleteCmdTests.cs
@@ -1,4 +1,5 @@
-﻿using Nca.Domain.Features.DataDefinitions.Delete;
+﻿using Nca.Domain.Entities.Definitions;
+using Nca.Domain.Features.DataDefinitions.Delete;
 
 namespace Nca.Tests.Domain.Features.DataDefinitions;
 
@@ -11,14 +12,40 @@ public class DataDefinitionsDeleteCmdTests
     {
         _handler = new DataDefinitionsDeleteCmdHandler(_db);
     }
-    
+
     [Fact]
     public async Task Should_delete_from_db()
     {
-        var cmd = new DataDefinitionsDeleteCmd();
+        var dataDefinition1 = new DataDefinition
+        {
+            Id = 1,
+            Name = "Def1",
+            Fields = new List<FieldDefinition> { new() { Id = 1 } }
+        };
+        var dataDefinition2 = new DataDefinition
+        {
+            Id = 2,
+            Name = "Def2",
+            Fields = new List<FieldDefinition> { new() { Id = 2 } }
+        };
+        var dataDefinition3 = new DataDefinition
+        {
+            Id = 3,
+            Name = "Def3",
+            Fields = new List<FieldDefinition> { new() { Id = 3 } }
+        };
+
+        _db.DataDefinitions.Add(dataDefinition1);
+        _db.DataDefinitions.Add(dataDefinition2);
+        _db.DataDefinitions.Add(dataDefinition3);
+        await _db.SaveChangesAsync();
+
+        var cmd = new DataDefinitionsDeleteCmd { Ids = [1, 3] };
 
         await _handler.ExecuteAsync(cmd);
 
-        throw new NotImplementedException();
+        Assert.Equal(1, _db.DataDefinitions.Count());
+        Assert.Equal(1, _db.FieldDefinitions.Count());
+        Assert.Equal("Def2", _db.DataDefinitions.Single(x => x.Id == 2).Name);
     }
 }
