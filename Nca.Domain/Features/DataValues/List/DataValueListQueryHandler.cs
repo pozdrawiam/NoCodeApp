cebdff7f1e5ca83619b4dd95b646f033e42bdb73
Dawid Tr <witam@tuta.io>
2024-06-11 19:39:52 +0200

    Data list query implementation

--- Nca.Domain/Features/DataValues/List/DataValueListQueryHandler.cs
+++ Nca.Domain/Features/DataValues/List/DataValueListQueryHandler.cs
@@ -1,10 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Nca.Domain.Entities.Definitions;
+using Nca.Domain.Entities.Values;
+
 namespace Nca.Domain.Features.DataValues.List;
 
 public class DataValueListQueryHandler(IDb db)
     : IQueryHandler<DataValueListQuery, DataValueListQueryResult>
 {
-    public Task<DataValueListQueryResult> ExecuteAsync(DataValueListQuery query)
+    public async Task<DataValueListQueryResult> ExecuteAsync(DataValueListQuery query)
     {
-        throw new NotImplementedException();
+        DataDefinition? definition = await db.DataDefinitions.FindAsync(query.DataDefinitionId);
+
+        if (definition == null)
+            return new DataValueListQueryResult();
+
+        FieldValue[] fieldValues = await db.DataValues.Where(x => x.DataDefinitionId == definition.Id).SelectMany(x => x.Fields).ToArrayAsync();
+
+        List<List<string>> rows = fieldValues.GroupBy(x => x.DataValueId)
+            .Select(valuesGroup => definition.Fields
+                .OrderBy(x => x.Sequence)
+                .Select(x => x.Id)
+                .Select(fieldDefId => valuesGroup.FirstOrDefault(x => x.FieldDefinitionId == fieldDefId)?.Value ?? "")
+                .ToList())
+            .ToList();
+
+        return new DataValueListQueryResult { Values = rows };
     }
 }
