e3f71a90863e8f3c86e33f0473a5e922231d1bff
David <witam@tuta.io>
2024-06-16 12:41:51 +0200

    Test class reformat

--- Nca.Tests/Architecture/CqsTests.cs
+++ Nca.Tests/Architecture/CqsTests.cs
@@ -6,7 +6,7 @@ namespace Nca.Tests.Architecture;
 public class CqsTests
 {
     private const string DomainAssemblyName = "Nca.Domain";
-    
+
     [Fact]
     public void Cmd_handlers_should_implement_interface()
     {
@@ -19,16 +19,16 @@ public class CqsTests
 
             if (iType == null)
                 Assert.Fail($"No interface {nameof(ICmdHandler<object>)} for cmd handler {type.Name}");
-            
+
             if (!iType.IsGenericType)
                 Assert.Fail($"$Type {iType.Name} is not generic in cmd handler {type.Name}");
-            
+
             var gType = iType.GetGenericTypeDefinition().GetGenericArguments()[0];
 
             if (!gType.Name.EndsWith("Cmd"))
                 Assert.Fail($"Invalid Cmd type name: {type.Name} : {iType.Name}<{gType.Name}>");
         }
-        
+
         Assert.NotEmpty(cmdHandlerTypes);
     }
 
@@ -45,7 +45,7 @@ public class CqsTests
 
             if (iType == null)
                 Assert.Fail($"No interface {nameof(IQueryHandler<object, object>)} for query handler {type.Name}");
-            
+
             if (!iType.IsGenericType)
                 Assert.Fail($"$Type {iType.Name} is not generic in query handler {type.Name}");
 
@@ -61,10 +61,10 @@ public class CqsTests
             {
                 Assert.Fail(
                     $"Invalid Query type name: {type.Name} : {iType.Name}<{queryType.Name}, {resultType.Name}>"
-                    );
+                );
             }
         }
-        
+
         Assert.NotEmpty(queryHandlerTypes);
     }
 }
