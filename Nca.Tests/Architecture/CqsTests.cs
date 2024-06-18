8a036d6701d173dbc8a4f6cd2961d416cc520e4a
Dawid Tr <witam@tuta.io>
2024-06-18 21:19:55 +0200

    Messages refactor in cqs tests

--- Nca.Tests/Architecture/CqsTests.cs
+++ Nca.Tests/Architecture/CqsTests.cs
@@ -18,15 +18,15 @@ public class CqsTests
             var iType = type.GetInterfaces().FirstOrDefault(x => x.Name.Contains(nameof(ICmdHandler<object>)));
 
             if (iType == null)
-                Assert.Fail($"No interface {nameof(ICmdHandler<object>)} for cmd handler {type.Name}");
+                Assert.Fail($"No interface '{nameof(ICmdHandler<object>)}' for cmd handler '{type.Name}'");
 
             if (!iType.IsGenericType)
-                Assert.Fail($"$Type {iType.Name} is not generic in cmd handler {type.Name}");
+                Assert.Fail($"$Type '{iType.Name}' is not generic in cmd handler '{type.Name}'");
 
             var gType = iType.GetGenericTypeDefinition().GetGenericArguments()[0];
 
             if (!gType.Name.EndsWith("Cmd"))
-                Assert.Fail($"Invalid Cmd type name: {type.Name} : {iType.Name}<{gType.Name}>");
+                Assert.Fail($"Invalid Cmd type name: '{type.Name} : {iType.Name}<{gType.Name}>'");
         }
 
         Assert.NotEmpty(cmdHandlerTypes);
@@ -62,24 +62,22 @@ public class CqsTests
                 .FirstOrDefault(x => x.Name.Contains(nameof(IQueryHandler<object, object>)));
 
             if (iType == null)
-                Assert.Fail($"No interface {nameof(IQueryHandler<object, object>)} for query handler {type.Name}");
+                Assert.Fail($"No interface '{nameof(IQueryHandler<object, object>)}' for query handler '{type.Name}'");
 
             if (!iType.IsGenericType)
-                Assert.Fail($"$Type {iType.Name} is not generic in query handler {type.Name}");
+                Assert.Fail($"$Type '{iType.Name}' is not generic in query handler '{type.Name}'");
 
             var gArgs = iType.GetGenericTypeDefinition().GetGenericArguments();
             var queryType = gArgs[0];
 
             if (!queryType.Name.EndsWith("Query"))
-                Assert.Fail($"Invalid Query type name: {type.Name} : {iType.Name}<{queryType.Name}, ...>");
+                Assert.Fail($"Invalid Query type name: '{type.Name} : {iType.Name}<{queryType.Name}, ...>'");
 
             var resultType = gArgs[1];
 
             if (!resultType.Name.EndsWith("Result"))
             {
-                Assert.Fail(
-                    $"Invalid Query type name: {type.Name} : {iType.Name}<{queryType.Name}, {resultType.Name}>"
-                );
+                Assert.Fail($"Invalid Query type name: '{type.Name} : {iType.Name}<{queryType.Name}, {resultType.Name}>'");
             }
         }
 
