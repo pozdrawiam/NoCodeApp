26aabca4a81f72f1897cbe763efd394d070d24a9
David <witam@tuta.io>
2024-06-19 21:42:42 +0200

    Sqlite provider

---
+++ Nca.Infrastructure/Storage/SqliteDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nca.Domain.Entities;
+
+namespace Nca.Infrastructure.Storage;
+
+public class SqliteDb(string connectionString) 
+    : Db(GetSqliteDbOptions(connectionString))
+{
+    private static DbContextOptions<Db> GetSqliteDbOptions(string connectionString)
+    {
+        return new DbContextOptionsBuilder<Db>()
+            .UseSqlite(connectionString)
+            .Options;
+    }
+}
