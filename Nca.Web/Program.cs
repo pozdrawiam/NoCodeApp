f1461daedebf7a7e5802fc64038e5869f08f2651
David <witam@tuta.io>
2024-06-19 21:55:04 +0200

    Add db in web app

--- Nca.Web/Program.cs
+++ Nca.Web/Program.cs
@@ -1,4 +1,10 @@
+using Nca.Domain.Entities;
+using Nca.Infrastructure.Storage;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IDb, SqliteDb>(_ => new SqliteDb(builder.Configuration["DbConnection"]!));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
