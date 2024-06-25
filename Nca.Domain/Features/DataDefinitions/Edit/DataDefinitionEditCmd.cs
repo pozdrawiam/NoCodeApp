04a11c5bebad3f26b3f5ba3df11af3670c365526
David <witam@tuta.io>
2024-06-25 22:31:36 +0200

    Definition edit handler

---
+++ Nca.Domain/Features/DataDefinitions/Edit/DataDefinitionEditCmd.cs
@@ -0,0 +1,14 @@
+﻿namespace Nca.Domain.Features.DataDefinitions.Edit;
+
+public class DataDefinitionEditCmd
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public IList<FieldDto> Fields { get; set; } = new List<FieldDto>();
+    
+    public class FieldDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+    }
+}
---
+++ Nca.Domain/Features/DataDefinitions/Edit/DataDefinitionEditHandler.cs
@@ -0,0 +1,21 @@
+﻿using Nca.Core.Exceptions;
+
+namespace Nca.Domain.Features.DataDefinitions.Edit;
+
+public class DataDefinitionEditHandler(IDb db) 
+    : ICmdHandler<DataDefinitionEditCmd>
+{
+    public async Task ExecuteAsync(DataDefinitionEditCmd cmd)
+    {
+        var entity = await db.DataDefinitions.FindAsync(cmd.Id);
+
+        if (entity == null)
+            throw new NotFoundException();
+        
+        entity.Name = cmd.Name;
+
+        db.DataDefinitions.Add(entity);
+
+        await db.SaveChangesAsync();
+    }
+}
