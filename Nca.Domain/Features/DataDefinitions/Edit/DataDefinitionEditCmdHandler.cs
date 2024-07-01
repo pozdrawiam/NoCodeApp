7f2038a079d2d25460827732858bb3aca34651be
David <witam@tuta.io>
2024-07-01 22:23:57 +0200

    Editing definition fields

--- Nca.Domain/Features/DataDefinitions/Edit/DataDefinitionEditCmdHandler.cs
+++ Nca.Domain/Features/DataDefinitions/Edit/DataDefinitionEditCmdHandler.cs
@@ -1,4 +1,6 @@
-﻿using Nca.Core.Exceptions;
+﻿using Microsoft.EntityFrameworkCore;
+using Nca.Core.Exceptions;
+using Nca.Domain.Entities.Definitions;
 
 namespace Nca.Domain.Features.DataDefinitions.Edit;
 
@@ -7,13 +9,48 @@ public class DataDefinitionEditCmdHandler(IDb db)
 {
     public async Task ExecuteAsync(DataDefinitionEditCmd cmd)
     {
-        var entity = await db.DataDefinitions.FindAsync(cmd.Id);
+        var entity = await db.DataDefinitions
+            .Include(x => x.Fields)
+            .FirstOrDefaultAsync(x => x.Id == cmd.Id);
 
         if (entity == null)
             throw new NotFoundException();
         
         entity.Name = cmd.Name;
+        
+        UpdateFields(cmd, entity);
 
         await db.SaveChangesAsync();
+    }
+
+    private static void UpdateFields(DataDefinitionEditCmd cmd, DataDefinition entity)
+    {
+        var fieldsToRemove = entity.Fields
+            .Where(f => cmd.Fields.All(cf => cf.Id != f.Id))
+            .ToList();
+        
+        foreach (var field in fieldsToRemove)
+        {
+            entity.Fields.Remove(field);
+        }
+        
+        foreach (var cmdField in cmd.Fields)
+        {
+            var existingField = entity.Fields.FirstOrDefault(f => f.Id == cmdField.Id);
+
+            if (existingField != null)
+            {
+                existingField.Name = cmdField.Name;
+            }
+            else
+            {
+                var newField = new FieldDefinition
+                {
+                    Id = cmdField.Id,
+                    Name = cmdField.Name
+                };
+                entity.Fields.Add(newField);
+            }
+        }
     }
 }
